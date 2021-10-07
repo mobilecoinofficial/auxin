7514a3b1376dd3b3d24db0bef3616b7e589c6580
technillogue <technillogue@gmail.com>
2021-10-07 14:25:39 -0400

    healthcheck

--- healthcheck.sh
+++ healthcheck.sh
@@ -2,6 +2,6 @@
 set -o errexit -o pipefail -o xtrace
 tested=${1:-+12513552703}
 number=$(ls state/data | head -1)
-auxin/auxin_cli -u $number send -m '/printerfact' $tested
-auxin/auxin_cli -u $number receive | tee output
-grep -i printer output
+target/debug/auxin-cli -u $number send -m '/ping' $tested
+target/debug/auxin-cli -u $number receive | tee output
+grep -s "pong" output
