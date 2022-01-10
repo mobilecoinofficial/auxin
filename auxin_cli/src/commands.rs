3af66faee9123198942e2efb9deb5e501dea8610
Millie C <gyro@pop-os.localdomain>
2022-01-10 14:47:27 -0500

    Version returned by --version should automatically track version in Cargo.toml now.

--- auxin_cli/src/commands.rs
+++ auxin_cli/src/commands.rs
@@ -29,7 +29,7 @@ use serde::{Deserialize, Serialize};
 use crate::{initiate_attachment_downloads, AttachmentPipelineError, ATTACHMENT_TIMEOUT_DURATION};
 
 pub const AUTHOR_STR: &str = "Forest Contact team";
-pub const VERSION_STR: &str = "0.1.7";
+pub const VERSION_STR: &str = env!("CARGO_PKG_VERSION");
 
 pub const JSONRPC_VER: &str = "2.0";
 
