18070c362e3dc14af4ff616461d5b4c3d3ef1ea6
Diana <5275194+DianaNites@users.noreply.github.com>
2022-03-31 15:11:21 -0400

    Fix bug with default Authorization header

--- auxin_cli/src/main.rs
+++ auxin_cli/src/main.rs
@@ -243,9 +243,8 @@ pub async fn async_main(exit_oneshot: tokio::sync::oneshot::Sender<i32>) -> Resu
 		headers.insert(
 			"Authorization",
 			header::HeaderValue::from_str(&format!(
-				"Basic {}:{}",
-				account.phone(),
-				account.password()
+				"Basic {}",
+				base64::encode(format!("{}:{}", account.phone(), account.password()))
 			))?,
 		);
 		let cert = reqwest::Certificate::from_pem(auxin::SIGNAL_TLS_CERT.as_bytes())?;
